27268df8fa3aacd23c2d127a60d5909dfe29204f
Artem Galaydin <a.galaydin@yandex.ru>
2023-01-02 13:43:15 +0300

    exercises

---
+++ books/ex00.py
@@ -0,0 +1,9 @@
+st = "Где это? Кто это? Когда это?"
+one = st[0:8]
+two = st[9:17]
+three = st[18:28]
+l = []
+l.append(one)
+l.append(two)
+l.append(three)
+print(l)
---
+++ books/ex01.py
@@ -0,0 +1,4 @@
+l = ["Рыжая", "лиса", "перепрыгнула", "через", "низкий", "забор", "."]
+result = " ".join(l)
+result = result.strip()
+print(result)
