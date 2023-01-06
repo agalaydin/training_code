c233330ebff7168d062e92bf046b969a996678f9
Artem Galaydin <a.galaydin@yandex.ru>
2023-01-06 21:06:30 +0300

    Piscine C

---
+++ C/exam/ft_print_numbers.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void	ft_print_numbers(void)
+{
+	char c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		write(1, &c, 1);
+		c++;
+	}
+	write(1, "\n", 1);
+
+}
+
+int main()
+{
+	ft_print_numbers();
+}
