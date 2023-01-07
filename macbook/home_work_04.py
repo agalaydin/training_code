6e5381a4a360391c305b098f0a59f9c9f91478ec
macbook <a.galaydin>
2023-01-07 12:16:11 +0300

    lessons

--- macbook/home_work_04.py
+++ macbook/home_work_04.py
@@ -1,7 +1,16 @@
 fruits = ["яблоко", "банан", "киви", "арбуз"]
 for count, value in enumerate(fruits, start=1):
     print(f'{count}.{value}')
-print(fruits)
+#print((f'{fruits:>5}')
+
+#print(fruits)
+
+
+
+
+
+
+
 #fruits = sorted(fruits.items(), key=lambda x: x[-1])
 #for key, value in d:
 #    print('{0:>10} {1:<2}'.format(key, value))
---
+++ macbook/test.py
@@ -0,0 +1,5 @@
+width = 5
+number = 123
+#print(f'{number:>{width}}')
+
+print("a string {0:>5}".format(foo))
