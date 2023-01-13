2ee1877f0f70472c9476e28fb8ac6012faf3aac3
Artem Galaydin <a.galaydin@yandex.ru>
2023-01-13 18:40:22 +0300

    exercises

---
+++ books/example_00048.py
@@ -0,0 +1,23 @@
+n =  input("введите число: ")
+n = int(n)
+
+if n % 2 == 0:
+    print("n - чётное.")
+else:
+    print("n - нечётное.")
+
+n = input("введите число: ")
+n = int(n)
+
+if n % 2 == 0:
+    print("n - чётное.")
+else:
+    print("n - нечётное.")
+
+n = input("введите число: ")
+n = int(n)
+
+if n % 2 == 0:
+    print("n - чётное.")
+else:
+    print("n - нечётное.")
