82d9ba0088dcaa826a1993627637c3955edf1163
macbook <a.galaydin>
2023-01-14 15:28:29 +0300

    re

--- macbook/example_06.py
+++
@@ -1,9 +0,0 @@
-a = list()
-a.append(int(input("Введите пераое число: ")))
-a.append(int(input("Введите второе число: ")))
-a.append(int(input("Введите третье число: ")))
-s = 0
-for i in a:
-    if i >= 0:
-        s += i
-print(s)
--- macbook/test.py
+++
@@ -1,5 +0,0 @@
-width = 5
-number = 123
-#print(f'{number:>{width}}')
-
-print("a string {0:>5}".format(foo))
