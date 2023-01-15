17e25324101f4f57fdc2cc078961765332519401
Artem Galaydin <a.galaydin@yandex.ru>
2023-01-15 10:06:10 +0300

    lessons

---
+++ macbook/lesson_00023.py
@@ -0,0 +1,9 @@
+# количества цифр в числе
+
+var_1 = int(input("Введите число: "))
+count = 0
+while var_1 > 0:
+    count = count + 1
+    var_1 = var_1 // 10
+print("Количество цифр в числе:", count)
+
