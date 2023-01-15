8ed501562299a44b5ce346474fa781d251ddf52e
Artem Galaydin <a.galaydin@yandex.ru>
2023-01-15 11:24:38 +0300

    lessons

---
+++ macbook/lesson_00032.py
@@ -0,0 +1,8 @@
+# проверка дубликатов
+
+def check_duplicate(lst):
+    return len(lst) != len(set(lst))
+
+print(check_duplicate([51, 37, 3, 45, 5, 49, 77]))
+print(check_duplicate([1, 3, 3]))
+print(check_duplicate([11, 2, 88, 4, 16]))
