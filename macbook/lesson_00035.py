54c267e7672cecfcfcb3813d3ed6159c26a658b7
Artem Galaydin <a.galaydin@yandex.ru>
2023-01-15 11:52:31 +0300

    lessons

---
+++ macbook/lesson_00035.py
@@ -0,0 +1,10 @@
+# определяем класс
+
+class MyClass:
+    my_int_var = 3
+    my_float_var = 3.14
+    my_str_var = 'Python'
+
+# обращаемся к атрибутам объекта
+
+print("{inst.my_str_var}-Питончик".format(inst=MyClass()))
