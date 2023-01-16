f811649135e110f300bfaab46fe69826a24247a0
Artem Galaydin <a.galaydin@yandex.ru>
2023-01-16 20:52:09 +0300

    lessons

---
+++ macbook/lesson_00073.py
@@ -0,0 +1,19 @@
+x = 10
+y = 20
+z = 30
+if x == 10:
+    print(x)
+elif y == 20:
+    print(y)
+else:
+    print(z)
+
+a = 1
+b = 2
+c = 3
+if a == 1:
+    print(a)
+if b == 2:
+    print(b)
+else:
+    print(c)
