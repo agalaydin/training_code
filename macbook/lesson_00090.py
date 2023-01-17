a0161545ba2ebca7b060c495d2ff7356c244c39c
Artem Galaydin <a.galaydin@yandex.ru>
2023-01-17 20:30:08 +0300

    lessons

---
+++ macbook/lesson_00090.py
@@ -0,0 +1,12 @@
+musicians = {"Ziver":
+              ["Beverly Hills",
+               "Зелёные волны",
+               "Credo"],
+             "Filatov & Karas":
+              ["Остаться с тобой",
+               "Движ",
+               "Amore Море, Goodbye"],
+             "Чеботина":
+              ["Солнце Монако"]
+            }
+print(musicians)
