f4956940481f3c670a4ccc2218ca994c9851b13b
Artem Galaydin <a.galaydin@yandex.ru>
2023-01-18 20:30:57 +0300

    Piscine C

---
+++ C/libft/ft_lstnew.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaarmand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/17 15:11:24 by kaarmand          #+#    #+#             */
+/*   Updated: 2021/10/22 13:52:43 by kaarmand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (0);
+	new->next = 0;
+	new->content = content;
+	return (new);
+}
