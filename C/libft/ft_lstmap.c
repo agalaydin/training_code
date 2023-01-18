893e3c0987c6b762467a000a6102037336698825
Artem Galaydin <a.galaydin@yandex.ru>
2023-01-18 20:30:50 +0300

    Piscine C

---
+++ C/libft/ft_lstmap.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaarmand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/17 15:10:39 by kaarmand          #+#    #+#             */
+/*   Updated: 2021/10/22 13:52:31 by kaarmand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*value;
+	t_list	*temp;
+
+	if (!lst || !f)
+		return (0);
+	new = ft_lstnew(f(lst->content));
+	if (!new)
+		return (0);
+	value = new;
+	lst = lst->next;
+	while (lst)
+	{
+		temp = ft_lstnew(f(lst->content));
+		if (!temp)
+		{
+			ft_lstclear(&new, del);
+			return (0);
+		}
+		value->next = temp;
+		value = temp;
+		lst = lst->next;
+	}
+	return (new);
+}
