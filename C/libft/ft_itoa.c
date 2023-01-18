4c51fa83bb4610e475ef5dc5bbf65b7229663f97
Artem Galaydin <a.galaydin@yandex.ru>
2023-01-18 20:29:50 +0300

    Piscine C

---
+++ C/libft/ft_itoa.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaarmand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/22 10:49:19 by kaarmand          #+#    #+#             */
+/*   Updated: 2021/10/22 13:37:41 by kaarmand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+static void	ft_check(int *n, int *sign, int *value)
+{
+	if (*n == -2147483648)
+	{
+		*n = *n + 1;
+		*sign = -1;
+		*value = 1;
+		*n = *n * -1;
+	}
+	else if (*n < 0)
+	{
+		*sign = -1;
+		*n = *n * -1;
+		*value = 0;
+	}
+	else if (*n >= 0)
+	{
+		*sign = 1;
+		*value = 0;
+	}
+}
+
+static int	ft_length(int n)
+{
+	int	len;
+
+	len = 0;
+	while (n > 9)
+	{
+		n = n / 10;
+		len++;
+	}
+	len++;
+	return (len);
+}
+
+static void	ft_write(char *str, int len, int n, int value)
+{
+	while (n > 9)
+	{
+		str[len--] = (n % 10) + '0' + value;
+		n = n / 10;
+		value = 0;
+	}
+	str[len] = n + '0';
+}
+
+char	*ft_itoa(int n)
+{
+	int		sign;
+	int		value;
+	int		len;
+	char	*str;
+
+	ft_check(&n, &sign, &value);
+	len = ft_length(n);
+	if (sign == -1)
+	{
+		str = malloc((len + 2) * sizeof(char));
+		if (!str)
+			return (0);
+		len++;
+		str[0] = '-';
+	}
+	else
+	{
+		str = malloc((len + 1) * sizeof(char));
+		if (!str)
+			return (0);
+	}
+	str[len--] = '\0';
+	ft_write(str, len, n, value);
+	return (str);
+}
