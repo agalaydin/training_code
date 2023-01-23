85db716b342ccb5112a558d038097aee38b9773f
Artem Galaydin <a.galaydin@yandex.ru>
2023-01-23 18:03:12 +0300

    lessons

---
+++ macbook/lesson_00129.py
@@ -0,0 +1,12 @@
+def de_dupe(items):
+    result = []
+    for item in items:
+        if item not in result:
+            result.append(item)
+    return result
+
+numbers = [1, 2, 2, 2, 3, 1, 2, 5, 5]
+
+de_duped_nums = de_dupe(numbers)
+print(de_duped_nums)
+
