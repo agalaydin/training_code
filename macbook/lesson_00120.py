33ab7b66405c3a0aca03c077af93852615fcc53b
Artem Galaydin <a.galaydin@yandex.ru>
2023-01-23 17:25:19 +0300

    lessons

---
+++ macbook/lesson_00120.py
@@ -0,0 +1,8 @@
+def flatten(nested_list):
+    return [item for sublist in nested_list for item in sublist]
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+numbers = flatten(matrix)
+
+print(numbers)
