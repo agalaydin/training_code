707459abc27a38bed0e038d9a7d0b37dccf19437
Artem Galaydin <a.galaydin@yandex.ru>
2023-01-25 21:57:12 +0300

    lessons

---
+++ C/libft/ft_strtrim.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaarmand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/17 15:53:04 by kaarmand          #+#    #+#             */
+/*   Updated: 2021/10/17 15:53:06 by kaarmand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t		size_s1;
+
+	if (!s1 || !set)
+		return (NULL);
+	while (ft_strchr(set, *s1) && *s1 != '\0')
+		s1++;
+	size_s1 = ft_strlen((char *)s1);
+	while (ft_strchr(set, s1[size_s1]) && size_s1 != 0)
+		size_s1--;
+	return (ft_substr((char *)s1, 0, size_s1 + 1));
+}
