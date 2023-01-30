54f40b1e804d7f97977e159c451dd76d4d324cb5
Artem Galaydin <a.galaydin@yandex.ru>
2023-01-30 18:23:49 +0300

    exercises

---
+++ books/example_00084.py
@@ -0,0 +1,14 @@
+a, b, c = int(input()), int(input()), int(input())
+if a == b:
+    if b == c:
+        print(3)
+    else:
+        print(2)
+else:
+    if a == c:
+        print(2)
+    else:
+        if b == c:
+            print(2)
+        else:
+            print(0)
