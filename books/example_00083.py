94c5efd696627a627f7ead0dc3f6f8d5f1b98ef2
Artem Galaydin <a.galaydin@yandex.ru>
2023-01-30 18:20:49 +0300

    exercises

---
+++ books/example_00083.py
@@ -0,0 +1,11 @@
+a, b, c = int(input()), int(input()), int(input())
+if a == b == c:
+    print(3)
+elif a == b != c:
+    pritn(2)
+elif a != b == c:
+    pritn(2)
+elif a == c != b:
+    pritn(2)
+else:
+    print(0)
