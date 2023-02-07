0f9de89eb23fd552d67d8897475328ab0e1a3fd8
Artem Galaydin <a.galaydin@yandex.ru>
2023-02-07 08:36:15 +0300

    lessons

---
+++ macbook/lesson_00172.py
@@ -0,0 +1,10 @@
+a = {1:'peanut', 2:'butter', 3:'jelly', 4:'time'}
+b = {1:'fish', 2:'chips'}         
+c = {1: ['peanut','butter','jelly','time'], 2:['fish','chips']}
+d = {1: ['fish','chips'], 2:['peanut','butter','jelly','time']}
+
+my_dict = a | b # python 3.9+
+print(my_dict)
+
+my_dict_2 = {**a, **b}
+print(my_dict_2)
