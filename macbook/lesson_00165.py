d792b4cc8e3751046140b3f00fedaef5bc0a1803
Artem Galaydin <a.galaydin@yandex.ru>
2023-02-07 08:02:26 +0300

    lessons

--- macbook/lesson_00165.py
+++ macbook/lesson_00165.py
@@ -1,11 +1,14 @@
 import operator
 
-# слияние двух словарей в один
+# слияние двух словарей в один в Python 3.9 и выше
 
 a = {'a':500, 'b':600, 'c':700}
 b = {'d':400, 'e':300, 'f':200, 'g':100}
 d = a | b
 print(d)
+
+# Сортировка по значению
+
 result = dict(sorted(d.items(), key=operator.itemgetter(1)))
 print(result)
 result = dict(sorted(d.items(), key=operator.itemgetter(1), reverse=True))
