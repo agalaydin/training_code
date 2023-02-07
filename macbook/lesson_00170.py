a575f3d51e3491a50e50925985eaa61d175cbb0d
Artem Galaydin <a.galaydin@yandex.ru>
2023-02-07 08:25:50 +0300

    lessons

---
+++ macbook/lesson_00170.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+Объединение словарей в Python 2
+использование метода update()
+'''
+a = {1:'peanut', 2:'butter', 3:'jelly', 4:'time'}
+b = {1:'fish', 2:'chips'}         
+c = {1: ['peanut','butter','jelly','time'], 2:['fish','chips']}
+d = {1: ['fish','chips'], 2:['peanut','butter','jelly','time']}
+
+x = a.copy()
+x.update(b)
+print(x)
