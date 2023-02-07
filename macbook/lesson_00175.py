3b6c5bfe791b1b18c755108b5b29889810f9a8d0
Artem Galaydin <a.galaydin@yandex.ru>
2023-02-07 08:51:52 +0300

    lessons

---
+++ macbook/lesson_00175.py
@@ -0,0 +1,6 @@
+# использование **
+date_info = {'year': "2020", 'month': "01", 'day': "01"}
+track_info = {'artist': "Beethoven", 'title': 'Symphony No 5'}
+filename = "{year}-{month}-{day}-{artist}-{title}.txt".format(**date_info, **track_info)
+print(filename)
+
