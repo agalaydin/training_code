b82532d64fbb0f51a282e49ef330e6181cc6dec8
Artem Galaydin <a.galaydin@yandex.ru>
2023-02-08 11:51:59 +0300

    lessons

--- macbook/lesson_00176.py
+++ macbook/lesson_00176.py
@@ -1,6 +1,15 @@
+# Слияние нескольких словарей
+
 dict_a = {1:10, 2:20}
 dict_b = {3:30, 4:40}
 dict_c = {5:50, 6:60}
 
 result = {}
-for d in (dict_a)
+for d in (dict_a, dict_b, dict_c):
+    result.update(d)
+print(result)
+
+# вариант со "звёздочками"
+
+res = {**dict_a, **dict_b, **dict_c}
+print(res)
