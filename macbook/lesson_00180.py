38ad78ae404b95a9ac6b7d16bdf769048e6a9e00
Artem Galaydin <a.galaydin@yandex.ru>
2023-02-09 17:43:56 +0300

    lessons

--- macbook/lesson_00180.py
+++ macbook/lesson_00180.py
@@ -2,7 +2,17 @@
 # Палиндром — это слово или фраза, которые одинаково читаются слева направо 
 # и справа налево
 
+# сравниваем строку с её обратной версией, 
+# для чего можно использовать встроенную функцию reversed
+
 def is_palindrome(string):
     return string == ''.join(reversed(string))
+
+print(is_palindrome('abba'))
+
+# используем срезы
+
+def is_palindrome(string):
+    return string == string[::-1]
 
 print(is_palindrome('abba'))
