687e7c7788a5dfc6e48d6ae976449c3089f315bd
Artem Galaydin <a.galaydin@yandex.ru>
2023-02-09 17:34:54 +0300

    lessons

--- macbook/lesson_00179.py
+++ macbook/lesson_00179.py
@@ -1,6 +1,8 @@
 def pascal_triangle(n):
-    row = [1]
-    y = [0]
-    for x in range(max(n, 0)):
-        print(row)
-        row = [left + right for left,]
+   row = [1]
+   y = [0]
+   for x in range(max(n, 0)):
+      print(row)
+      row = [left + right for left, right in zip(row + y, y + row)]
+
+pascal_triangle(6)
