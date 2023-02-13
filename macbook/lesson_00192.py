3c3a3cba0f1285687f8295ba37cd669d16041273
Artem Galaydin <a.galaydin@yandex.ru>
2023-02-13 14:33:42 +0300

    lessons

---
+++ macbook/lesson_00192.py
@@ -0,0 +1,18 @@
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(lst)
+lst.append('cтрока')
+print(lst)
+lst[4] = 189
+print(lst)
+lst.append(['a', 'b', 'c', 'hello'])
+print(lst)
+lst[-3] = (1, 6, 89)
+print(lst)
+print(lst[0])
+print(lst[-1])
+lst.remove(189)
+print(lst)
+print(lst.count(1))
+
+
+
