528c9075e64aa61fa93aea64a8a4b4cbd04417e0
Artem Galaydin <a.galaydin@yandex.ru>
2023-02-16 19:37:14 +0300

    lessons

--- macbook/lesson_00198.py
+++ macbook/lesson_00198.py
@@ -1,3 +1,13 @@
 def calc(a, b, operation):
     # Задаём дефолтное значение возвращаемого результата
     result = 'Операция не поддерживается'
+
+    if operation == '+':
+        result = a + b
+    elif operation == '-':
+        result = a - b
+    elif operation == '*':
+        result = a * b
+    elif operation == '/':
+        # Проверка деления на ноль
+
