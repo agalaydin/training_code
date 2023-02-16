e0a81ad2dde859035b2950710a6520fbef2e7e60
Artem Galaydin <a.galaydin@yandex.ru>
2023-02-16 19:41:52 +0300

    lessons

--- macbook/lesson_00198.py
+++ macbook/lesson_00198.py
@@ -10,4 +10,20 @@ def calc(a, b, operation):
         result = a * b
     elif operation == '/':
         # Проверка деления на ноль
+        if b is not 0:
+            result = a / b
+        else:
+            result = 'Делить на 0 нельзя!'
 
+    return result
+
+if __name__ == '__main__':
+    # Проверяем корректные значения
+    print(calc(30, 15, '+'))
+    print(calc(30, 15, '-'))
+    print(calc(30, 15, '*'))
+    print(calc(30, 15, '/'))
+    # Проверяем проверку деления на ноль
+    print(calc(30, 0, '/'))
+    # Проверяем неподдерживаемую операцию
+    print(calc(30, 15, '%'))
