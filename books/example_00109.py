40d83ee3d533ecca08938774f0737370883ac151
Artem Galaydin <a.galaydin@yandex.ru>
2023-02-25 20:21:12 +0300

    exercises

---
+++ books/example_00109.py
@@ -0,0 +1,5 @@
+# Преобразование разделённого запятыми списка в строку
+my_list1 = ["Python", "JavaScript", "C++"]
+my_list2 = ["Java", "Flutter", "Swift"]
+print("My favourite Programming Languages are" , ", ".join(my_list1))
+print(", ".join(my_list2))
