35136dc8d2e57c0c3a0c0803bd9dee778c4b07e7
Artem Galaydin <a.galaydin@yandex.ru>
2023-02-27 22:40:22 +0300

    exercises

--- books/example_00109.py
+++ books/example_00109.py
@@ -2,6 +2,8 @@
 my_list1 = ["Python", "JavaScript", "C++"]
 my_list2 = ["Java", "Flutter", "Swift"]
 my_list3 = ["C#", "PHP", "Pascal"]
+my_list4 = ["C", "C++", "C#"]
 print("My favourite Programming Languages are" , ", ".join(my_list1))
 print(", ".join(my_list2))
 print(", ".join(my_list3))
+print(", ".join(my_list4))
