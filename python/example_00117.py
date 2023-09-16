004337b96f99a9db909676f5f4adbbd9d01aeaa9
Artem Galaydin <a.galaydin@yandex.ru>
2023-09-16 17:07:18 +0300

    Python in 7 hours

---
+++ python/example_00117.py
@@ -0,0 +1,10 @@
+"""Python in 7 hours."""
+
+import os
+import time
+
+os.system('start https://www.youtube.com')
+
+time.sleep(5)
+
+os.startfile('C://Program Files/4KDownload/4kvideodownloader/4kvideodownloader.exe')
