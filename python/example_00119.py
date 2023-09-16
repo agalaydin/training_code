6fc3e6b6f62b75da82b1f943465a11ee69645a98
Artem Galaydin <a.galaydin@yandex.ru>
2023-09-16 17:23:59 +0300

    Python in 7 hours

---
+++ python/example_00119.py
@@ -0,0 +1,17 @@
+"""Python in 7 hours."""
+
+x = ''
+
+while len(x) < 5:
+    y = input('Ввод данных: ')
+    if y == 'o':
+        continue
+    if y == 'l':
+        break
+
+    x += y
+
+else:
+    print(x)
+
+print('Программа работает дальше')
